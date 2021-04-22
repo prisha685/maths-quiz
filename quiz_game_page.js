4c20bc7688e65f1b763f76faf9232d226c711b79
prisha685 <77448105+prisha685@users.noreply.github.com>
2021-04-22 21:03:18 +0530

    Add files via upload

--- quiz_game_page.js
+++ quiz_game_page.js
@@ -9,3 +9,39 @@ function send() {
     document.getElementById("output").innerHTML=row;
     document.getElementById("number1").value="";
     document.getElementById("number2").value="";
+}
+question_turn="player1";
+answer_turn="player2";
+function send() {
+    get_answer=document.getElementById("input_check_box").value;
+        if(get_answer==actual_answer) {
+            if(answer_turn=="player1"){
+                update_player1_score=update_player1_score+1;
+                document.getElementById("player1_score").innerHTML=update_player1_score;
+            }
+            else{
+                update_player2_score=update_player2_score+1;
+                document.getElementById("player2_score").innerHTML=update_player2_score;
+            }
+            if(question_turn=="player2"){
+               question_turn="player2";
+                document.getElementById("player_question").innerHTML="Question Turn -" + player2_name;
+            }
+            else{
+                question_turn="player1";
+                document.getElementById("player_question").innerHTML="Question Turn -" + player1_name;
+            }
+            if(answer_turn=="player2"){
+               answer_turn="player2";
+                 document.getElementById("player_answer").innerHTML="Answer Turn -" + player2_name;
+             }
+             else{
+                 answer_turn="player1";
+                 document.getElementById("player_answer").innerHTML="Answer Turn -" + player1_name;
+             }
+     
+    
+        }
+
+    }
+  
