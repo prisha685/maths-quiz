6c9505dea2f7dc885bddc538a07c7ff0000d3442
prisha685 <77448105+prisha685@users.noreply.github.com>
2021-05-08 19:15:48 +0530

    Add files via upload

--- mathsq.js
+++ mathsq.js
@@ -1,9 +1,9 @@
 function addUser() {
-    player1_name=document.getElementById("player_1_name").value;
-    player2_name=document.getElementById("player_2_name").value;
-    localStorage.setItem("player_1_name", player1_name);
-    localStorage.setItem("player_2_name", player2_name);
-    window.location.replace("quiz_game_page.html");
+    player1_name=document.getElementById("player1_name_input").value;
+    player2_name=document.getElementById("player2_name_input").value;
+    localStorage.setItem("player1_name", player1_name);
+    localStorage.setItem("player2_name", player2_name);
+    window.location="quiz_game_page.html";
  
  }
  
